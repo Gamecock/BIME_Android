55a85802c15488cb8cd5673c1dddfe0870990861
Mike Finch <mikefinch87@gmail.com>
2018-10-08 23:06:24 -0500

    Added login screen

--- app/src/main/java/com/bime/digitalid/MainActivity.java
+++ app/src/main/java/com/bime/digitalid/MainActivity.java
@@ -1,5 +1,7 @@
 package com.bime.digitalid;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,13 +14,17 @@ import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String LOGIN_TOKEN = "com.bime.digitalid.TOKEN";
+    public static final Integer LOGIN_CODE = 123;
     private String TAG = "Main";
+
+    private String token = null;   //TODO: Determine proper way to handle tokens
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "Created Main Activity");
         setContentView(R.layout.activity_main);
-
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,7 +32,27 @@ public class MainActivity extends AppCompatActivity {
                 generateQR("Hello World");
             }
         });
+        if (null == token) {
+            // no token so we need to login
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            Log.d(TAG, "Starting Activity for result");
+            startActivityForResult(loginIntent, LOGIN_CODE);
+        }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode,data);
+        if (requestCode == LOGIN_CODE){
+            if (resultCode == Activity.RESULT_OK) {
+                token = data.getStringExtra(LOGIN_TOKEN);
+                //TODO: error handling
+            }
+        }else {
+            Log.e(TAG, "No idea what is request code:"+requestCode);
+        }
+    }
+
 
     private void generateQR(String content) {
         try {
