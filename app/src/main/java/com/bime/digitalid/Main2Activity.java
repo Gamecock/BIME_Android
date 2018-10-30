570440d97225bee6e0038e9a353ac8f0e1de3db6
Dunnb18 <dunnb18@students.ecu.edu>
2018-10-30 19:58:01 -0400

    second screen layout

---
+++ app/src/main/java/com/bime/digitalid/Main2Activity.java
@@ -0,0 +1,16 @@
+package com.bime.digitalid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+
+    }
+
