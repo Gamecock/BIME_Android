aa6dcf2b3bf0b1009aacee28ee9b4fdfda05708c
Mike Finch <mikefinch87@gmail.com>
2018-11-11 20:23:03 -0500

    Make getRequestQueue private

--- app/src/main/java/com/bime/digitalid/MyRequestQueue.java
+++ app/src/main/java/com/bime/digitalid/MyRequestQueue.java
@@ -31,7 +31,7 @@ public class MyRequestQueue {
         return mInstance;
     }
 
-    public RequestQueue getRequestQueue() {
+    private RequestQueue getRequestQueue() {
         if (mRequestQueue == null) {
             // getApplicationContext() is key, it keeps you from leaking the
             // Activity or BroadcastReceiver if someone passes one in.
