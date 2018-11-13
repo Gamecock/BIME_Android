d10a28abebf10b4f48c64aecf48f81cf7de0e11f
Mike Finch <mikefinch87@gmail.com>
2018-11-13 15:06:14 -0500

    Display QR Code for Meals
    	Uses the BannerId returned from the server to create the meal URL
            Refactors the QR display into it's own activity.

--- app/src/main/java/com/bime/digitalid/MainActivity.java
+++ app/src/main/java/com/bime/digitalid/MainActivity.java
@@ -2,21 +2,16 @@ package com.bime.digitalid;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import android.widget.Button;
-import android.widget.ImageView;
-import com.google.zxing.BarcodeFormat;
-import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String LOGIN_TOKEN = "com.bime.digitalid.TOKEN";
     public static final String BANNER_ID = "com.bime.digitalid.BANNER";
+    public static final String QR_TEXT = "com.bime.digitalid.QR_TEXT";
+    public static final String QR_STATUS = "com.bime.digitalid.QR_STATUS";
+
 
     public static final Integer LOGIN_CODE = 123;
 
@@ -42,19 +40,20 @@ public class MainActivity extends AppCompatActivity {
     private String token = null;   //TODO: Determine proper way to handle tokens
     private String bannerID = null;
 
+    private  Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "Created Main Activity");
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button2);
+        button = findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Commented out pending PR for multiple buttons
 //                getQRText("/greeting?name=Brian");
-                String BannerId = "B0223456";
-                getMealTicket(BannerId);
+                getMealTicket(bannerID);
             }
         });
         if (null == token) {
@@ -78,8 +77,7 @@ public class MainActivity extends AppCompatActivity {
         }else {
             Log.e(TAG, "No idea what is request code:"+requestCode);
         }
-        String BannerId = "B0223456";
-        getRemainingMeals(BannerId);
+        getRemainingMeals(bannerID);
 
     }
 
@@ -99,14 +97,15 @@ public class MainActivity extends AppCompatActivity {
                             Log.e(TAG, e.getMessage());
                         }
                         Log.d(TAG, "Number of meals available:"+meals);
-                        Button button = findViewById(R.id.button);
                         button.setText(new StringBuilder("Get Meal, ").append(meals).append(" remaining").toString());
 
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, "That didn't work!"+error.toString());
+                Log.e(TAG, "That didn't work!" +error.toString());
+                Log.e(TAG, "Response Code: " +error.networkResponse.statusCode);
+                Log.e(TAG, "Error message:" +error.getMessage());
                 //TODO: Display error for user.
             }
         });
@@ -132,13 +131,15 @@ public class MainActivity extends AppCompatActivity {
                         } catch (JSONException e) {
                             Log.e(TAG, e.getMessage());
                         }
-                        generateQR(content);
+                        generateQR(content, 200);
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Log.e(TAG, "That didn't work!"+error.toString());
-                //TODO: Display error for user.
+                Log.e(TAG, "Error message"+ error.getMessage());
+                Log.e(TAG, "Response Code: "+error.networkResponse.statusCode);
+                generateQR(error.getMessage(),error.networkResponse.statusCode);
             }
         });
 
@@ -147,48 +148,12 @@ public class MainActivity extends AppCompatActivity {
         MyRequestQueue.getInstance(this.getApplicationContext()).addToRequestQueue(request);
     }
 
+    private void generateQR(String content, int code) {
 
-    private void getQRText(String resource){
-
-        String url = service+server+resource;
-
-        // Request a JSON response from the provided URL.
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        String content = null;
-                        try {
-                            content = response.getString("content");
-                        } catch (JSONException e) {
-                            Log.e(TAG, e.getMessage());
-                        }
-                        generateQR(content);
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, "That didn't work!"+error.toString());
-                //TODO: Display error for user.
-            }
-        });
-
-        // Add the request to the RequestQueue.
-        Log.d(TAG, "Sending request to server:"+url);
-        MyRequestQueue.getInstance(this.getApplicationContext()).addToRequestQueue(request);
-    }
-
-    private void generateQR(String content) {
-
-        try {
-            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
-            Bitmap bitmap = barcodeEncoder.encodeBitmap(content, BarcodeFormat.QR_CODE, 400, 400);
-            Log.d(TAG, "Created barcode with content:"+content);
-//            ImageView imageViewQrCode = findViewById(R.id.qr_view);
-//            imageViewQrCode.setImageBitmap(bitmap);
-        } catch(Exception e) {
-            Log.e(TAG, e.getMessage() );
-        }
+        Intent intent = new Intent(this, QRActivity.class);
+        intent.putExtra(QR_TEXT, content);
+        intent.putExtra(QR_STATUS, code);
+        startActivity(intent);
     }
 
 
